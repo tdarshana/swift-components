958349b8ab58267da1995c7d884441f76180520e
MaxIndu <maxindupro@gmail.com>
2020-12-15 00:29:30 +0800

    Camera App Example

---
+++ CameraApp/CameraApp/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Tharindu Darshana on 14/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView:UIImageView!
+    @IBOutlet var button:UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        button.backgroundColor = .systemBlue
+        button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+    }
+    
+    @IBAction func didTapButton(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {return}
+        imageView.image = image
+    }
+}
